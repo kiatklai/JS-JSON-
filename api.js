ed1060df384ac153965697d41c11497248723823
kiatklai <likhitthaworn.k@gmail.com>
2023-07-07 21:43:06 +0900

    apiからデータ取得

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
-//https://jsonplaceholder.typicode.com/users
+const list = document.getElementById("list")
+
+let output=''
+
+async function getDataFromAPI(){
+  const url = "https://jsonplaceholder.typicode.com/users"
+  const res=await fetch(url)
+  const json=await res.json()
+  json.forEach(item=>{
+    output+="<li>"+item.name+"</li>"
+  })
+  list.innerHTML=output
+}
+getDataFromAPI()
